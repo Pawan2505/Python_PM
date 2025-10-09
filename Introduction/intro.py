876fd0a4bf163d586e13ad22e85b06c9032eb3ef
pawan2505 <pawanaktu2gmail.com>
2025-10-09 17:46:40 +0530

    ops

--- Introduction/intro.py
+++ Introduction/intro.py
@@ -1,2 +1,186 @@
 
+# comment: comment is used for documentation
+# single line comment
+# output function
+'''
+comment: comment is used for documentation
+single line comment
+output function
+
+'''
+"""
+comment: comment is used for documentation
+single line comment
+output function
+"""
+
+'''
+multi line comment
+'''
+
+
+'''
 print("Hello World")
+print("Hello World drgtru7i87o09[0[p; ]]")
+'''
+
+# print(40+50)
+
+'''
+name = "Jenith"
+age = 16
+print(name)
+print(age)
+
+'''
+
+'''
+name = "Jenith"
+age = 16
+print("My name is :",name)
+print("Age is :",age)
+'''
+
+'''
+name = "Jenith"
+age = 16
+print("My name is :",name,"Age is :",age)
+'''
+
+
+# password = input("Enter Password : ")
+
+# print("Your Password : ",password)
+
+# name = "Pawan"
+# age = 25
+
+# print("Name :",name)
+# print("Datatype of Name :",type(name))
+# print("Age :",age)
+# print("Datatype of Age :",type(age))
+
+# is_student = True
+
+# print(is_student)
+# print(type(is_student))
+
+# z = 2 + 3j
+
+# print(z)
+# print(type(z))
+
+
+# num1 = 20
+
+# num2 = 30
+
+# result  = 30>20
+
+# print(result)
+# print(type(result))
+
+# num = "12"
+
+# print(num)
+# print(type(num))
+
+# y = int(num)
+
+# print(y)
+# print(type(y))
+
+# z =float(y)
+
+# print(z)
+# print(type(z))
+
+# x = 10
+
+# a = str(x)
+
+# print(a)
+# print(type(a))
+
+# r = 12
+
+# print(id(r))
+
+
+# # q = 14
+
+# q = r
+
+# print(id(q))
+
+
+#Arithmetic Operators -> +,-,*,/,%,//,**
+
+# x = 4
+# y = 2
+
+# print("Add : ",x+y)
+# print("Sub : ",x-y)
+# print("mul : ",x*y)
+# print("div : ",x/y)
+# print("rem : ",x%y)
+# print("floor : ",x//y)
+# print("power : ",x**y)
+
+
+# Comparison (Relational) Operators : <, >, ==, >=, <=,!=
+
+
+# x= 23
+# y = 19
+
+# print(x>y)
+# print(x<y)
+# print(x==y)
+# print(x!=y)
+# print(x>=y)
+# print(x<=y)
+
+# Logical Operators -> and,or,not
+
+# x= 23
+# y = 19
+# z = 2
+
+# print(x>y or x < z)
+# print(not(x>y and x > z))
+
+
+
+# j = 10
+
+# # j = j+20
+# j +=20
+
+# print(j)
+
+# j = 10
+
+# # j = j%3
+# j %=3
+
+# print(j)
+
+# Identity Operators
+
+# a = 10
+# b = 10
+
+# print(a is b)
+# a = 10
+# b = 20
+
+# print(a is not b)
+
+# Membership Operators
+
+
+# print("Pa" in "Pawan")
+# print("Pz" not in "Pawan")
+
+# print("Apples", "Orange", "Mango", sep=", ", end=" are fruits!")
